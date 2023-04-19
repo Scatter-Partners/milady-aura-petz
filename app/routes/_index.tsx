2fe9f9a17d29fdf0adad0c09e6f2a479f89cb8ac
PolyDegen <polydegen@protonmail.com>
2023-04-19 18:08:14 +0530

    try adding siwe

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,10 +1,31 @@
 import type { V2_MetaFunction } from "@remix-run/react";
+   import { useSubmit } from '@remix-run/react';
+import { useCallback } from "react";
+   import { SiweMessage } from 'siwe';
 
 export const meta: V2_MetaFunction = () => {
   return [{ title: "New Remix App" }];
 };
 
 export default function Index() {
+      const submit = useSubmit();
+
+      function authenticate = useCallback(() => {
+         // create siwe message
+         const message = await new SiweMessage({ siweMessageOptions });
+         // sign siwe message
+         const signature = await signer.signMessage(message);
+
+         const formData = new FormData();
+         formData.append('message', message);
+         formData.append('signature', signature);
+         submit(formData, {
+            action: {YOUR_LOGIN_ACTION},
+            method: 'post',
+            replace: true,
+         });
+      }, [submit])
+
   return (
     <div>
       <h1 className="text-3xl text-amber-600 font-bold underline">
